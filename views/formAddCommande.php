6c6a061f815dce9a6c600e1816805d8a44a37c16
jeffsama72 <fouassier.jeanfrancois72@gmail.com>
2017-02-16 11:49:11 +0100

    formaddcommande

---
+++ views/formAddCommande.php
@@ -0,0 +1,20 @@
+<form method="post" action="addCommande">
+	<p>
+		<label for="produit">Quel produit souhaitez-vous commander</label><br />
+		<select name="produit" id="produit">
+			<?php foreach ($listeProduit as $produit): ?>
+				<option value="id">libelle</option>
+			<?php endforeach ?>
+		</select>
+		<select name="produit" id="produit">
+			<?php foreach ($listeProduit as $produit): ?>
+				<option value="id">libelle</option>
+			<?php endforeach ?>
+		</select>
+		<select name="produit" id="produit">
+			<?php foreach ($listeProduit as $produit): ?>
+				<option value="id">libelle</option>
+			<?php endforeach ?>
+		</select>
+	</p>
+</form>
